30d460d72c1eb0a8f3376f2b2eb6a9163c132407
centyh <97asmo@gmail.com>
2022-08-23 13:42:44 +0300

    Sound Volume Slider to Settings

--- 3D Tactic/Assets/Scripts/Asmo/Volume.cs
+++ 3D Tactic/Assets/Scripts/Asmo/Volume.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Volume : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Slider volumeSlider = null;
+
+
+    private void Start()
     {
-        
+        LoadValue();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    public void SaveVolume()
     {
-        
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValue();
+    }
+
+    void LoadValue()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
     }
 }
