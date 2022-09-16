c88b6eb0dcaa6d406b5630ba130dc06276662ec0
centyh <97asmo@gmail.com>
2022-09-16 13:28:51 +0300

    jotaki paskaaa

--- 3D Tactic/Assets/Scripts/Asmo/MenuScript.cs
+++ 3D Tactic/Assets/Scripts/Asmo/MenuScript.cs
@@ -5,31 +5,31 @@ using UnityEditor;
 
 public class MenuScript
 {
-    [MenuItem("Tools/Assign Tile Material")]
+    //[MenuItem("Tools/Assign Tile Material")]
 
-    //Tool - Asetetaan kaikille "Tile" tagillisille objekteille Tile Materiaali.
-    public static void AssignTileMaterial()
-    {
-        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
-        Material material = Resources.Load<Material>("Tile");
+    ////Tool - Asetetaan kaikille "Tile" tagillisille objekteille Tile Materiaali.
+    //public static void AssignTileMaterial()
+    //{
+    //    GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+    //    Material material = Resources.Load<Material>("Tile");
 
-        foreach(GameObject t in tiles)
-        {
-            t.GetComponent<Renderer>().material = material;
-        }
-    }
+    //    foreach(GameObject t in tiles)
+    //    {
+    //        t.GetComponent<Renderer>().material = material;
+    //    }
+    //}
 
-    [MenuItem("Tools/Assign Tile Script")]
+    //[MenuItem("Tools/Assign Tile Script")]
 
-    //Tool - Asetetaan kaikille "Tile" tagillisille objekteille Tile-Scripti.
-    public static void AssignTileScript()
-    {
-        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
-        Material material = Resources.Load<Material>("Tile");
+    ////Tool - Asetetaan kaikille "Tile" tagillisille objekteille Tile-Scripti.
+    //public static void AssignTileScript()
+    //{
+    //    GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+    //    Material material = Resources.Load<Material>("Tile");
 
-        foreach (GameObject t in tiles)
-        {
-            t.AddComponent<Tile>();
-        }
-    }
+    //    foreach (GameObject t in tiles)
+    //    {
+    //        t.AddComponent<Tile>();
+    //    }
+    //}
 }
